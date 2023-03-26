89b55ab1a4b762792a90c59c5f0933f686cb3a7b
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2023-03-26 13:00:42 +0800

    Fix BSLeafAnimNode offset

--- include/RE/B/BSLeafAnimNode.h
+++ include/RE/B/BSLeafAnimNode.h
@@ -12,17 +12,17 @@ namespace RE
 		~BSLeafAnimNode() override;  //00
 
 		// override (BSFadeNode)
-		const NiRTTI*   GetRTTI() const override;                           // 02
-		NiObject*       CreateClone(NiCloningProcess& a_cloning) override;  // 17
-		void            LoadBinary(NiStream& a_stream) override;            // 18
-		void            LinkObject(NiStream& a_stream) override;            // 19
-		bool            RegisterStreamables(NiStream& a_stream) override;   // 1A
-		void            SaveBinary(NiStream& a_stream) override;            // 1B
-		bool            IsEqual(NiObject* a_object) override;               // 1C
+		const NiRTTI* GetRTTI() const override;                           // 02
+		NiObject*     CreateClone(NiCloningProcess& a_cloning) override;  // 17
+		void          LoadBinary(NiStream& a_stream) override;            // 18
+		void          LinkObject(NiStream& a_stream) override;            // 19
+		bool          RegisterStreamables(NiStream& a_stream) override;   // 1A
+		void          SaveBinary(NiStream& a_stream) override;            // 1B
+		bool          IsEqual(NiObject* a_object) override;               // 1C
 #ifndef SKYRIM_CROSS_VR
 		// The following are virtual functions past the point where VR compatibility breaks.
 		void            OnVisible(NiCullingProcess& a_process) override;  // 34
-		BSLeafAnimNode* AsLeafAnimNode() override;                          // 3F
+		BSLeafAnimNode* AsLeafAnimNode() override;                        // 3F
 #endif
 	private:
 		float sqrDistanceToCamera;  //158
@@ -31,9 +31,9 @@ namespace RE
 		float runningTime;          //164
 		float previousTimer;        //168
 	};
-#ifndef ENABLE_SKYRIM_VR
+#ifdef ENABLE_SKYRIM_VR
 	static_assert(sizeof(BSLeafAnimNode) == 0x128);
-#elif !defined(ENABLE_SKYRIM_AE) && !defined(ENABLE_SKYRIM_SE)
+#else
 	static_assert(sizeof(BSLeafAnimNode) == 0x170);
 #endif
 }
