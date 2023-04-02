7601e177264d831f94217f8fde226cf1924d9f3e
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2023-04-02 12:01:28 +0800

    Fixed DirectionHandler size

--- include/RE/D/DirectionHandler.h
+++ include/RE/D/DirectionHandler.h
@@ -15,6 +15,8 @@ namespace RE
 		bool CanProcess(InputEvent* a_event) override;      // 01
 		bool ProcessKinect(KinectEvent* a_event) override;  // 02
 		bool ProcessButton(ButtonEvent* a_event) override;  // 05
+
+		char unk10[32];
 	};
-	static_assert(sizeof(DirectionHandler) == 0x10);
+	static_assert(sizeof(DirectionHandler) == 0x30);
 }
